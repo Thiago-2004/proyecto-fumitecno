b9e0c0431d24751294448ce1401ecc3347a01651
thiago-ricon <thiagoricon987@gmail.com>
2024-08-25 20:12:51 -0300

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <NavbarCustom />
         <main>
           <Routes>
-            <Route path="/Seccion" element={<Navigate to="/Fumitecno" />} />
+            <Route path="/" element={<Navigate to="/Fumitecno" />} />
             <Route path='/Fumitecno' element={<Seccion />} />
             <Route path='/Servicios' element={<Servicios/>} />
             <Route path='/Nosotros' element={<Nosotros/>} />
